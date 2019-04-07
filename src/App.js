f3f713e17c3488e8817bceb5161498b6ee903d8a
- Pocko <Ruslan_Nikolov@epam.com>
2019-04-07 19:36:51 +0300

    test git

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn Reactttttt
+            Learn React
           </a>
         </header>
       </div>
