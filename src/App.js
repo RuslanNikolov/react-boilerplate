ae8367fe28751f55bea68cff3410d45497290d7f
- Pocko <Ruslan_Nikolov@epam.com>
2019-04-07 19:35:45 +0300

    test again

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn Reacttt
+            Learn Reactttttt
           </a>
         </header>
       </div>
