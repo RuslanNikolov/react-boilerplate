652edf14ce9533c0474f94262b8f9fbd65514aad
- Pocko <Ruslan_Nikolov@epam.com>
2019-04-16 22:28:38 +0300

    structure

---
+++ create_component.sh
@@ -0,0 +1,6 @@
+mkdir -p src/components/$1
+touch src/components/$1/$1.style.js
+touch src/components/$1/$1.tsx
+touch src/components/$1/$1.test.tsx
+touch src/components/$1/$1.context.ts
+touch src/components/$1/$1.context.test.ts
